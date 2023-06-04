ee8cff8a7d4414dc1f8a2b755fdf2a51565ea267
anikolskaya <108408108+anikolskaya@users.noreply.github.com>
2023-06-04 16:44:01 +0300

    Update annotation_voting.py

--- annotation_voting.py
+++ annotation_voting.py
@@ -2,23 +2,29 @@ import pyranges as pr
 import pandas as pd
 import sys
 import argparse
+import os
+from pathlib import Path
 
 from io_ import *
 from stats_voting import *
-from io_.schemas import voted_BED
+from io_.schemas import voted_BED, rdc_pyranges_BED
+from io_.fileops import load_BED_annot
+pd.options.mode.chained_assignment = None
 
 parser = argparse.ArgumentParser(description='Annotate RNA-parts of contacts, perform a voting procedure and deplete contacts derived from ribosomal RNAs')
 parser.add_argument('rdc', type=str, help='Path to rna-dna contacts file')
 parser.add_argument('gene_annot', type=str, help='Path to gene annotation')
-parser.add_argument('keep_strand', type=bool, help='Whether to overlap intervals on the same strand (True) or the opposite one (False)')
-#parser.add_argument('ncpus',type=int, default=1, help='How many cpus to use')
+parser.add_argument('--annot_file_format', nargs='?', default='GTP', type=str, help='Gene annotation file format: GTF or BED (default: GTP)')
+parser.add_argument('--outdir', nargs='?', type=str, default = './results', help='A folder in which to store results')
+#parser.add_argument('--ncpus', nargs='?', type=int, default=1, help='How many cpus to use')
 args = parser.parse_args()
 
 
 def run_annotation_and_voting(
                               gene_annot_path, 
                               contacts_path,
-                              keep_strand = True,
+                              annot_format = 'GTF',
+                              outdir = './results',
                               ncpus = 1):
     """
     Annotate RNA-parts of contacts, perform a voting procedure and deplete
@@ -32,9 +38,6 @@ def run_annotation_and_voting(
     
     contacts_path: str
         Path to an RDC-like file with RNA-DNA contacts
-        
-    keep_strand: bool
-        Whether to overlap intervals on the same strand (True) or the opposite one (False).
     
     ncpus: int
         How many cpus to use. 
@@ -49,27 +52,49 @@ def run_annotation_and_voting(
     """
 
     d = {}
-    gene_ann = load_gtf_restricted(gene_annot_path)
-    cnts = load_rdc(contacts_path, 
-                    ncpus=ncpus)
+    empty = []
     
-    if keep_strand:
-        d['selected_annot'], d['complement_annot'], d['no_annot'] = annotate_rdc(cnts, gene_ann, cpus = ncpus)
+    print(annot_format)
+    if annot_format == 'GTF':
+        gene_ann = load_gtf_restricted(gene_annot_path)
+    elif annot_format == 'BED':
+        gene_ann = load_BED_annot(gene_annot_path)
     else:
-        d['complement_annot'], d['selected_annot'], d['no_annot'] = annotate_rdc(cnts, gene_ann, cpus = ncpus)
+        raise Exception('Gene annotation format is not supported')
+        
+    cnts = load_rdc(contacts_path, header = None,
+                    ncpus=ncpus)
+    cnts.drop(['rna_cigar', 'dna_cigar'], axis=1, inplace=True)
+    cnts = pr.PyRanges(cnts)
+    
+    d['selected_annot'], d['complement_annot'], d['no_annot'] = annotate_rdc(cnts, gene_ann, cpus = ncpus)
     
     d = dict(map(lambda item: (item[0], item[1].drop(like="annot$").as_df()), d.items()))
     
     d['voted_annot'] = vote(d['selected_annot'])
+    print('Voting is completed..')
     
     d['voted_noribo'] = remove_ribo(d['voted_annot'])
-    d['voted_noribo'].drop(['gene_length', 'count', 'density'], axis = 1, inplace = True)
+    print('Ribosomal contacts are removed..')
+
+    pathlib.Path(outdir).mkdir(parents=True, exist_ok=True)
+
+    for name, dataframe in d.items():
+        if not dataframe.empty:
+            dataframe.iloc[:, :11].to_csv(os.path.join(outdir, Path(args.rdc).stem + '_' + name + '.tab'), sep = '\t',
+                                                                                                                  header=voted_BED, 
+                                                                                                                  index=False)
+        else:
+            dataframe.to_csv(os.path.join(outdir, Path(args.rdc).stem + '_' + name + '.tab'))
+            empty.append(name)
+    
+    for empty_df in empty:
+        d.pop(empty_df, None)
 
     calculate_stats(d)
-    
-    d['voted_noribo'].columns = voted_BED
-    
-    return d
+    print('Stats are calculated..')
+
+    return 
 
     
 
@@ -152,4 +177,4 @@ def remove_ribo(contacts,
     """
     return contacts[~contacts.gene_type.isin(ribo_list)]
 
-run_annotation_and_voting(args.gene_annot, args.rdc, args.keep_strand)
+run_annotation_and_voting(args.gene_annot, args.rdc, args.annot_file_format, args.outdir)
