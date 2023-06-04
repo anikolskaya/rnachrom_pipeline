504cd7a301e48f71ad43c766e44ce8209ec1d8b9
anikolskaya <108408108+anikolskaya@users.noreply.github.com>
2023-06-04 16:44:41 +0300

    Update fileops.py

--- io_/fileops.py
+++ io_/fileops.py
@@ -1,7 +1,7 @@
 import pyranges as pr
 import pandas as pd
 
-from .schemas import rdc_pyranges_BED, rdc_dtypes, voted_BED
+from .schemas import rdc_pyranges_BED, rdc_dtypes, voted_BED, annot_pyranges_BED
 
 def load_gtf_restricted(path):
     """
@@ -20,16 +20,43 @@ def load_gtf_restricted(path):
     try:
         ann = pr.read_gtf(path)
     except TypeError:
-        raise ("A GTF-like DataFrame is required")
+        print("A GTF-like DataFrame is required")
         
     ann.gene_length = ann.lengths()
     ann = ann[ann.Feature == 'gene'][['gene_name', 'gene_type', 'gene_length']]
     
     return ann
-    
+
+def load_BED_annot(path):
+    """
+    Read intervals and metadata from a bed file.
+
+    Parameters
+    ----------
+    path : str
+        Path to a bed file
+
+    Returns
+    -------
+    PyRanges
+
+    """
+    try:
+            ann = pd.read_csv(path, 
+                                          header = 0, 
+                                          index_col=None,
+                                          sep='\t',
+                                          names = annot_pyranges_BED)
+    except TypeError:
+            print("A BED-like DataFrame is required")
+
+    ann = pr.PyRanges(ann)
+    ann.gene_length = ann.lengths()
+
+    return ann[['gene_name', 'gene_type', 'gene_length']]
     
 
-def load_rdc(path, sort = True, ncpus = 1):
+def load_rdc(path, header = None, sort = True, ncpus = 1):
     """
     Read intervals and metadata from a RNA-DNA contacts file.
 
@@ -45,23 +72,17 @@ def load_rdc(path, sort = True, ncpus = 1):
 
     Returns
     -------
-    PyRanges
+    DataFrame
 
     """
     try:
         rdc = pd.read_csv(path,
-                       header=None,
+                       header=header,
                        index_col=None,
                        sep='\t',
                        names=rdc_pyranges_BED,
                        dtype=rdc_dtypes)
     except ValueError:
-        raise ("An RDC-like DataFrame is required")
-        
-    rdc.drop(['rna_cigar', 'dna_cigar'], axis=1, inplace=True)
-    rdc = pr.PyRanges(rdc)
-    
-    if sort:
-        rdc = rdc.sort(nb_cpu = ncpus)
-    
+        print("An RDC-like DataFrame is required")
+
     return rdc
