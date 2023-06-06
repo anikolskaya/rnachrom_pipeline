3f92ebb1c97c1384022f20a00d31be3c65f0b9bc
anikolskaya <108408108+anikolskaya@users.noreply.github.com>
2023-06-06 12:28:34 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@
 #python3 blacklisting.py -h
 #python3 annotation_voting.py -h
 
-python3 splicing.py test_files/test_contacts/SRR9201799.tab
+python3 splicing.py test_files/SRR9201799.tab
 python3 blacklisting.py ./results/SRR9201799_CIGAR_processed.tsv hg38
-python3 -W ignore annotation_voting.py ./results/SRR9201799_CIGAR_processed_blacklst.tsv test_files/test_annot/gencode.v43.annotation.gene.gtf --annot_file_format GTF
+python3 -W ignore annotation_voting.py ./results/SRR9201799_CIGAR_processed_blacklst.tsv test_files/gencode.v43.annotation.gene.gtf --annot_file_format GTF
