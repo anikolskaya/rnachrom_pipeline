e6536db19ed84b84ac89a5687fd497f54c330fcc
anikolskaya <108408108+anikolskaya@users.noreply.github.com>
2023-06-06 12:18:46 +0300

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,8 @@
+
+#python3 splicing.py -h
+#python3 blacklisting.py -h
+#python3 annotation_voting.py -h
+
+python3 splicing.py test_files/test_contacts/SRR9201799.tab
+python3 blacklisting.py ./results/SRR9201799_CIGAR_processed.tsv hg38
+python3 -W ignore annotation_voting.py ./results/SRR9201799_CIGAR_processed_blacklst.tsv test_files/test_annot/gencode.v43.annotation.gene.gtf --annot_file_format GTF
