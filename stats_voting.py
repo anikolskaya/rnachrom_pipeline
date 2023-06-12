f33aa633cb965dba8f3339b3c17f885c2f2dbd79
anikolskaya <108408108+anikolskaya@users.noreply.github.com>
2023-06-12 13:06:28 +0300

    Update stats_voting.py

--- stats_voting.py
+++ stats_voting.py
@@ -3,9 +3,10 @@ import pandas as pd
 import pathlib
 import os
 from natsort import natsort_keygen
+from pathlib import Path
 
 
-def calculate_stats(frames_d,
+def calculate_stats(frames_d, rdc_path = "rdc", 
                     outdir_path = './stats'):
     """
     Calculate statistics for annotation and voting processes.
@@ -15,8 +16,11 @@ def calculate_stats(frames_d,
     ----------
     frames_d: dict
         Dict from run_annotation_and_voting command
+
+    rdc_path: str
+         RDC filename will be used to generate files
         
-    outdir_path:
+    outdir_path: str
         Path to the output directory
     
     """
@@ -25,19 +29,19 @@ def calculate_stats(frames_d,
     
     frames_d['voted_annot'].sort_values('gene_name', ascending=True, inplace = True)
     
-    stats_by_gene_type(frames_d['voted_annot']).to_csv(os.path.join(outdir_path,'counts_gene_type.tab'),
+    stats_by_gene_type(frames_d['voted_annot']).to_csv(os.path.join(outdir_path, Path(rdc_path).stem + '_counts_gene_type.tab'),
                                                        index = True, 
                                                        sep = '\t')
     
-    density_by_gene(frames_d['voted_annot']).to_csv(os.path.join(outdir_path,'genes_density.tab'), 
+    density_by_gene(frames_d['voted_annot']).to_csv(os.path.join(outdir_path, Path(rdc_path).stem + '_genes_density.tab'), 
                                                     index = False, 
                                                     sep = '\t')
     
-    counts_by_gene(frames_d['voted_annot']).to_csv(os.path.join(outdir_path,'counts_by_genes.tab'), 
+    counts_by_gene(frames_d['voted_annot']).to_csv(os.path.join(outdir_path, Path(rdc_path).stem + '_counts_by_genes.tab'), 
                                                    index = True, 
                                                    sep = '\t')
     
-    counts_by_chr(frames_d).to_csv(os.path.join(outdir_path,'counts_by_chr.tab'), 
+    counts_by_chr(frames_d).to_csv(os.path.join(outdir_path, Path(rdc_path).stem + '_counts_by_chr.tab'), 
                                                index = True, 
                                                sep = '\t')
     return
