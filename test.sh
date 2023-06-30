187ff933d50bf6e50e11f99965a92138ba04369e
anikolskaya <108408108+anikolskaya@users.noreply.github.com>
2023-06-30 18:56:54 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,3 +6,6 @@
 python3 splicing.py test_files/SRR9201799.tab
 python3 blacklisting.py ./results/SRR9201799_CIGAR_processed.tsv hg38
 python3 -W ignore annotation_voting.py ./results/SRR9201799_CIGAR_processed_blacklst.tsv test_files/gencode.v43.annotation.gene.gtf --annot_file_format GTF
+
+
+python3 normalize_N2.py ./results/GRID_mES_pipe.chr19_voted_annot.tab /mnt/lustre/suvorova/tools/src/Smoother ./test_files/run_stereogene/cfg.cfg ./test_files/run_stereogene/hg38_chrsize.tab --filter_top_n 1 --filter_tail_n 1
