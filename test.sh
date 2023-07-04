c6bde97816dfdeedb30a29cfc4a184c859c1438c
anikolskaya <108408108+anikolskaya@users.noreply.github.com>
2023-07-04 11:31:10 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 #python3 splicing.py -h
 #python3 blacklisting.py -h
 #python3 annotation_voting.py -h
+#python3 splicing.py -h
 
 python3 splicing.py test_files/SRR9201799.tab
 python3 blacklisting.py ./results/SRR9201799_CIGAR_processed.tsv hg38
@@ -9,3 +10,4 @@ python3 -W ignore annotation_voting.py ./results/SRR9201799_CIGAR_processed_blac
 
 
 python3 normalize_N2.py ./results/GRID_mES_pipe.chr19_voted_annot.tab /mnt/lustre/suvorova/tools/src/Smoother ./test_files/run_stereogene/cfg.cfg ./test_files/run_stereogene/hg38_chrsize.tab --filter_top_n 1 --filter_tail_n 1
+python3 scaling.py rdc_norm_path chrsizes --threads 4
